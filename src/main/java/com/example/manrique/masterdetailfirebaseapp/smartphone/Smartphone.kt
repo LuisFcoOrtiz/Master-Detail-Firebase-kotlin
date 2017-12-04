a14f11552295a5841b45643567fc7bcbb3a4cf7e
Luis Fco Ortiz Manrique <luis.fco.om@gmail.com>
2017-12-04 16:04:28 +0100

    Uploading the main content

---
+++ src/main/java/com/example/manrique/masterdetailfirebaseapp/smartphone/Smartphone.kt
@@ -0,0 +1,8 @@
+package com.example.manrique.masterdetailfirebaseapp.smartphone
+
+/**
+ * Created by root on 20/11/17.
+ */
+data class Smartphone(val foto: String, val id: String, val nombre: String,  val web: String) {
+
+}//class only for data from smartphones
